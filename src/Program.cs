3931a2ecdad3e3a1e8f6e06c531a8e3dfb431ecf
geherious <vlaznev.04@mail.ru>
2024-05-09 17:58:20 +0300

    app logic

---
+++ src/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace TopContributors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string inputFile = "commits.txt";
+            const string outputFile = "result.txt";
+            const int numberOfPlaces = 3;
+
+            string projectPath = GetProjectPath();
+            string inputPath = Path.Combine(projectPath, inputFile);
+            string outputPath = Path.Combine(projectPath, outputFile);
+
+            if (!File.Exists(inputPath))
+            {
+                throw new FileNotFoundException($"Missing {inputFile} in src directory!");
+            }
+
+            string[] contributions = File.ReadAllLines(inputPath);
+
+            Rating rating = new Rating(numberOfPlaces); 
+            Dictionary<string, int> topContributors = rating.GetOrderedContributionRating(contributions);
+
+            StreamWriter output = new StreamWriter(outputPath);
+            output.Write(string.Join(" ", topContributors.Keys));
+            output.Close();
+        }
+
+        static string GetProjectPath()
+        {
+            string processPath = Directory.GetCurrentDirectory();
+            string projectPath = Directory.GetParent(processPath).Parent.Parent.FullName;
+            return projectPath;
+        }
+    }
+}
---
+++ src/Rating.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TopContributors
+{
+    internal class Rating
+    {
+        public int NumberOfPlaces { get; }
+
+        public Rating(int numberOfPlaces)
+        {
+            NumberOfPlaces = numberOfPlaces;
+        }
+
+        public Dictionary<string, int> GetOrderedContributionRating(string[] contributions)
+        {
+            Dictionary<string, int> bestContributors = contributions
+                // only unique entries
+                .Distinct()
+                .Where(IsValidEntry)
+                // take only name
+                .Select(x => x.Split()[0])
+                .GroupBy(name => name)
+                .OrderByDescending(g => g.Count())
+                .Take(NumberOfPlaces)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+             return bestContributors;
+        }
+
+        private bool IsValidEntry(string entry)
+        {
+            const string pattern = @"^[a-zA-Z_]\w*\s[a-z0-9]{7}\s\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.?\d*$";
+            Regex regex = new Regex(pattern);
+            
+            return regex.IsMatch(entry);
+        }
+    }
+}
